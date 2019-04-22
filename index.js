c8b18bf0bcfe791ea9c12e569ec273ead2f8bd9f
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-22 11:52:12 -0600

    finished and working

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world')
+let count = 0
+let element = document.getElementById('counter')
+let buttons = document.getElementsByTagName('button')
+function increase() {
+    count++
+    element.innerText = count
+}
+
+function decrease() {
+    count--
+    element.innerText = count
+}
+
+function reset() {
+    count = 0
+    element.innerText = "<mark>" + count + "</mark>"
+}
+
+function selectTheme(theme) {
+    document.body.className = theme
+    document.getElementsByTagName('main')[0].className = theme
+    for (i = 0; i < buttons.length; i ++) {
+        buttons[i].className = theme
+    }
+}
